2756e31c072bf25fa1c97d50a0c6d1df849cdda0
DESKTOP-3PIKUV6\86147 <2119318733@qq.com>
2022-04-26 15:26:55 +0800

    淘宝呗项目

---
+++ Mysql/app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import mysql from 'mysql'
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'project'
+});
+
+connection.connect((err) => {
+    if (!err) {
+        console.log('连接mysql成功')
+    }
+});
+
+const app = express()
+
+app.get('/getCategory', (req, res) => {
+    //解决跨域
+    res.set('Access-Control-Allow-Origin', '*')
+    connection.query('select * from test', (err, data, field) => {
+        if (!err) {
+            //返回查询数据
+            res.send(data)
+        }
+    })
+})
+
+app.listen(8000, () => {
+    console.log('服务器开启中')
+})
